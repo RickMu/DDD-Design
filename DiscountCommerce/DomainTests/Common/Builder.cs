2a0215cde5a23be670d8b452ec5aa63407bdbbbc
MSI\Rick <rick.mu@rokt.com>
2019-03-30 15:21:13 +1100

    unittests

--- DiscountCommerce/DomainTests/Common/Builder.cs
+++ DiscountCommerce/DomainTests/Common/Builder.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Domain;
 using Domain.Product;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace DomainTests.Common
 {
@@ -21,7 +23,25 @@ namespace DomainTests.Common
         public static Product GetProduct(Decimal basePrice, string attrbName, string[] values)
         {
             var attrb = GetProductAttribute(attrbName, values);
-            return new Product(basePrice,new[] {attrb}, null);
+            return new Product(basePrice,new List<ProductAttribute> {attrb}, null);
+        }
+
+        public static SelectedAttribute GetSelectedAttrbs(string name, string value)
+        {
+            return new SelectedAttribute(name, new ProductAttributeOption(value));
+        }
+
+        public static IList<SelectedAttribute> GetListSelectedAttrbs(string[] names, string[] values)
+        {
+            var nameAndValues = names.Zip(values, (n, v) => new {Name = n, Value = v}).ToList();
+            var retValue = new List<SelectedAttribute>();
+            foreach (var nameAndValue in nameAndValues)
+            {
+                var attrb = GetSelectedAttrbs(nameAndValue.Name, nameAndValue.Value);
+                retValue.Add(attrb);
+            }
+
+            return retValue;
         }
     }
 }
--- DiscountCommerce/DomainTests/Product/ProductTests.cs
+++ DiscountCommerce/DomainTests/Product/ProductTests.cs
@@ -1,27 +1,56 @@
+using System;
+using Domain;
 using Domain.Common.Exception;
 using Domain.Product;
 using DomainTests.Common;
 using FluentAssertions;
 using Xunit;
+using Xunit.Sdk;
 
 namespace DomainTests.ProductTests
 {
     public class ProductTests
     {
-        private Product product;
-
+        private Func<Product> product;
+        
         public ProductTests()
         {
-            product = Builder.GetProduct(1, "firstAttrb", new string[] {"1", "2", "3"});
+            product = () => Builder.GetProduct(1, "firstAttrb", new string[] {"1", "2", "3"});
         }
 
         [Fact]
         public void CannotAdd_AnotherAttribute_OfSameName()
         {
             var duplicateAttrb = Builder.GetProductAttribute("firstAttrb", new string[] {"value"});
-            product.Invoking(product =>
-                    product.AddAttribute(duplicateAttrb))
+            var product = this.product();
+            
+            product.Invoking(x =>
+                    x.AddAttribute(duplicateAttrb))
                 .Should().Throw<DomainException>().Where(e => e.Message.Contains($"{duplicateAttrb}"));
+        }
+        
+        [Fact]
+        public void AddProductAttribute_Should_Work()
+        {
+            var secondAttrb = Builder.GetProductAttribute("secondAttrb", new string[] {"value"});
+            var product = this.product();
+            product.AddAttribute(secondAttrb);    
+        }
+
+        [Theory]
+        [InlineData("2", "3", true)]
+        [InlineData("-1","5", false)]
+        public void AreAttributesCombinationValidChoice_Should_ReturnTrue(string firstOption, string secondOption, bool expected)
+        {
+            var product = this.product();
+            var secondAttribute = Builder.GetProductAttribute("secondAttrb", new string[] {"1","2","3"});
+            product.AddAttribute(secondAttribute);
+
+            var attrbCombination = Builder.GetListSelectedAttrbs(new string[] {"firstAttrb", "secondAttrb"},
+                new string[] {firstOption, secondOption});
+
+            product.AreAttributesCombinationValidChoice(attrbCombination)
+                .Should().Be(expected);
         }
     }
 }
