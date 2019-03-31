f720dd254254d40643ebcaa9b09df0f5202009ce
MSI\Rick <rick.mu@rokt.com>
2019-03-31 11:09:58 +1100

    fix tests

--- DomainTests/ProductSell/ProductPriceTests.cs
+++ DomainTests/ProductSell/ProductPriceTests.cs
@@ -25,7 +25,6 @@ namespace ProductSellsTests
             var newPrice = productPrice.CalculatePrice(count);
 
             newPrice.Should().Be((decimal) expected);
-            newPrice.Should().Be((decimal) productPrice.Discount);
         }
     }
 }
